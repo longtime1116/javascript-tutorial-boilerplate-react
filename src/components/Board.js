6c9b38c62860ee3f716d381771e2cee1faea7ee5
Tokinaga Shumo <longtime1116@gmail.com>
2018-08-02 19:21:20 +0900

    the change before the main tutorial

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import Square from './Square';
+import React from "react";
+import Square from "./Square";
 
 export default class Board extends React.Component {
   renderSquare(i) {
-    return <Square />;
+    return <Square value={i} />;
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = "Next player: X";
 
     return (
       <div>
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React from "react";
 
 export default class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: props.value
+    };
+  }
   render() {
-    return <button className="square">{/* TODO */}</button>;
+    return (
+      <button
+        className="square"
+        onClick={() => {
+          this.setState({ value: this.state.value + 1 });
+        }}
+      >
+        {this.state.value}
+      </button>
+    );
   }
 }
